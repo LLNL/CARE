dbff3ef5d4759ce3e9e39f95243276944382231a
Alan Dayton <dayton8@llnl.gov>
2020-06-25 10:19:51 -0700

    Update the blueos config

--- configs/lc/blueos/nvcc10.1.243_clang8.0.1.cmake
+++ configs/lc/blueos/nvcc10.1.243_clang8.0.1.cmake
@@ -1,5 +1,8 @@
 set(ENABLE_CUDA ON CACHE BOOL "Enable CUDA")
 set(CUDA_TOOLKIT_ROOT_DIR "/usr/tce/packages/cuda/cuda-10.1.243" CACHE PATH "Path to CUDA")
+set(CUDA_ARCH "sm_70" CACHE STRING "Set the CUDA virtual architecture")
+set(CUDA_CODE "compute_70" CACHE STRING "Set the CUDA actual architecture")
+
 set(NVTX_DIR "${CUDA_TOOLKIT_ROOT_DIR}" CACHE PATH "Path to NVTX")
 
 set(CMAKE_CXX_COMPILER "/usr/tce/packages/clang/clang-8.0.1/bin/clang++" CACHE FILEPATH "Path to clang++")
