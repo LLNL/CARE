2fb02490704c1ea201025d66ce6413bc586b3c9c
Alan Dayton <dayton8@llnl.gov>
2020-07-17 11:17:34 -0700

    Allow CUB_DIR to point to vanilla cub

--- cmake/libraries/FindCUB.cmake
+++ cmake/libraries/FindCUB.cmake
@@ -17,20 +17,20 @@ if (NOT TARGET cub)
    if (CUB_DIR)
       message(STATUS "CARE: Using external CUB")
 
-      set(CUB_PATH ${CUB_DIR}/include)
+      set(CUB_PATHS ${CUB_DIR} ${CUB_DIR}/include)
    else ()
       message(STATUS "CARE: Using CUB submodule")
 
       if (NOT EXISTS ${PROJECT_SOURCE_DIR}/tpl/cub/cub/cub.cuh)
          message(FATAL_ERROR "CARE: CUB submodule not initialized. Run 'git submodule update --init' in the git repository or set CUB_DIR to use an external build of CUB.")
       else ()
-         set(CUB_PATH ${PROJECT_SOURCE_DIR}/tpl/cub)
+         set(CUB_PATHS ${PROJECT_SOURCE_DIR}/tpl/cub)
       endif ()
    endif ()
 
    find_path(CUB_INCLUDE_DIR
              NAMES cub/cub.cuh
-             PATHS ${CUB_PATH}
+             PATHS ${CUB_PATHS}
              NO_DEFAULT_PATH
              NO_PACKAGE_ROOT_PATH
              NO_CMAKE_PATH
@@ -46,8 +46,12 @@ if (NOT TARGET cub)
 
    if (CUB_FOUND)
       set(CUB_INCLUDE_DIRS ${CUB_INCLUDE_DIR})
+      set(CUB_DEPENDS cuda)
 
       blt_register_library(NAME cub
-                           INCLUDES ${CUB_INCLUDE_DIR})
+                           INCLUDES ${CUB_INCLUDE_DIR}
+                           DEPENDS_ON ${CUB_DEPENDS})
+   else ()
+      message(FATAL_ERROR "CARE: CUB not found. Run 'git submodule update --init' in the git repository or set CUB_DIR to use an external build of CUB.")
    endif ()
 endif ()
