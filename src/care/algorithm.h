ac4c9f501ee3ecc3d9e1656632e84ed827b4ac64
Peter B. Robinson <robinson96@llnl.gov>
2021-03-17 15:59:06 -0700

    move implementation to decl.h to support inlining with compilation units that only use decl.h

--- src/care/algorithm.h
+++ src/care/algorithm.h
@@ -837,53 +837,6 @@ CARE_INLINE void CompressArray(care::host_device_ptr<T> & arr, const int arrLen,
    return CompressArray(RAJAExec(), arr, arrLen, removed, removedLen, noCopy);
 }
 
-/************************************************************************
- * Function  : InsertionSort
- * Author(s) : Rob Neely
- * Purpose   : Simple insertion sort function.  Should only be used on
- *             small arrays - otherwise use the qsort function from the
- *             standard C library.  Sorts in ascending order.
- ************************************************************************/
-template <typename T>
-CARE_HOST_DEVICE CARE_INLINE void InsertionSort(care::local_ptr<T> array, int len)
-{
-   if (len <= 1) {
-      return;
-   }
-
-   for (int i=1 ; i<len ; ++i) {
-      T tmp = array[i] ;
-      int j ;
-      for (j=i-1 ; (j >= 0) && (array[j] > tmp) ; --j) {
-         array[j+1] = array[j];
-      }
-      array[j+1] = tmp ;
-   }
-}
-/************************************************************************
- * Function  : sortLocal
- * Author(s) : Benjamin Liu
- * Purpose   : General sort routine to call from within RAJA loops.
- *             Sorts in ascending order.
- ************************************************************************/
-template <typename T>
-CARE_HOST_DEVICE CARE_INLINE void sortLocal(care::local_ptr<T> array, int len)
-{  
-   if (len > 1) {
-#if defined(__CUDA_ARCH__)
-      // TODO this should be replaced with a CUDA GPU sort implementation that
-      // is reasonable for longer arrays.
-      InsertionSort(array, len) ;
-#elif defined(__HIP_DEVICE_COMPILE__)
-      // TODO this should be replaced with a HIPCC GPU sort implementation that
-      // is reasonable for longer arrays.
-      InsertionSort(array, len) ;
-#else
-      // host compile case
-      std::sort(array.data(), array.data()+len) ;
-#endif
-   }
-}
 
 /************************************************************************
  * Function  : uniqLocal
@@ -1447,23 +1400,6 @@ CARE_INLINE void ArrayCopy(RAJA::seq_exec,
    CARE_SEQUENTIAL_LOOP(i, 0, n) {
       into[i+start1] = from[i+start2];
    } CARE_SEQUENTIAL_LOOP_END
-}
-
-/************************************************************************
- * Function  : ArrayCopy
- * Author(s) : Peter Robinson
- * Purpose   : Copies from one local_ptr into another. from and to
- *             should not have the same or overlapping memory addresses.
- * ************************************************************************/
-template<typename T>
-CARE_HOST_DEVICE CARE_INLINE void ArrayCopy(
-                           care::local_ptr<T> into,
-                           care::local_ptr<const T> from,
-                           int n, int start1, int start2)
-{
-   for (int i = 0; i < n; ++i)  {
-      into[i+start1] = from[i+start2];
-   }
 }
 
 /************************************************************************
