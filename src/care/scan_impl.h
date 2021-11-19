da2c02f2e2cc01f32d5139ca31bfabe6b43ba256
Benjamin T. Liu <liu15@llnl.gov>
2021-11-19 10:14:37 -0800

    Add check for inclusive_scan

--- src/care/scan_impl.h
+++ src/care/scan_impl.h
@@ -118,6 +118,53 @@ void exclusive_scan(chai::ManagedArray<T> data, //!< [in/out] Input data (output
 template <typename T, typename Exec, typename Fn, typename ValueType=T>
 void inclusive_scan(chai::ManagedArray<T> data, chai::ManagedArray<T> outData,
                     int size, Fn binop, bool inPlace) {
+   if (size > 0) {
+      if (inPlace) {
+         if (!data) {
+            printf("[CARE] Warning: Invalid arguments to care::inclusive_scan. If inPlace is true, data cannot be nullptr.\n");
+            return;
+         }
+      }
+      else {
+         if (!outData) {
+            printf("[CARE] Warning: Invalid arguments to care::inclusive_scan. If inPlace is false, outData cannot be nullptr.\n");
+            return;
+         }
+         else if (size > 1 && !data) {
+            printf("[CARE] Warning: Invalid arguments to care::inclusive_scan. If inPlace is false and size > 1, data cannot be nullptr.\n");
+            return;
+         }
+      }
+
+#if !defined(CHAI_DISABLE_RM)
+      // Bounds checking is only available with the resource manager because the ManagedArray::size() is not
+      // reliable if it is cast to a different template type.
+      const chai::PointerRecord* dataRecord = chai::ArrayManager::getInstance()->getPointerRecord((void *)data.cdata());
+      int dataSize = dataRecord->m_size/sizeof(T);
+
+      if (dataSize < size) {
+         const char* dataName = CHAICallback::getName(dataRecord);
+         if (dataName == nullptr) {
+            dataName = "array" ;
+         }
+         printf("[CARE] Warning: Invalid argument to care::inclusive_scan. Size %d < %d in input %s.\n", dataSize, size, dataName);
+      }
+
+      if (!inPlace) {
+         const chai::PointerRecord* outDataRecord = chai::ArrayManager::getInstance()->getPointerRecord((void *)outData.cdata());
+         int outDataSize = dataRecord->m_size/sizeof(T);
+
+         if (outDataSize < size) {
+            const char* outDataName = CHAICallback::getName(outDataRecord);
+            if (outDataName == nullptr) {
+               outDataName = "array" ;
+            }
+            printf("[CARE] Warning: Invalid argument to care::inclusive_scan. Size %d < %d in output %s.\n", outDataSize, size, outDataName);
+         }
+      }
+#endif
+   }
+
    CHAIDataGetter<T, Exec> D {};
    ValueType * rawData = D.getRawArrayData(data);
 
