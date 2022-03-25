80d7e9d1a5eceaddb26de7e1836986f0c5df974f
Benjamin T. Liu <liu15@llnl.gov>
2022-03-25 16:23:22 -0700

    Fix bug in GPU implementation of KeyValueSorter sort with non-zero start; also provide warning if non-zero start used with noCopy

--- src/care/KeyValueSorter_impl.h
+++ src/care/KeyValueSorter_impl.h
@@ -60,6 +60,10 @@ CARE_INLINE void sortKeyValueArrays(host_device_ptr<KeyT> & keys,
                                     const size_t start, const size_t len,
                                     const bool noCopy)
 {
+   if (noCopy && start > 0) {
+      printf("[CARE] Warning: sortKeyValueArrays. noCopy should not be set if start > 0 (%d)\n", (int)start);
+   }
+
    // Allocate space for the result
    host_device_ptr<KeyT> keyResult{len};
    host_device_ptr<ValueT> valueResult{len};
@@ -126,9 +130,9 @@ CARE_INLINE void sortKeyValueArrays(host_device_ptr<KeyT> & keys,
       values = valueResult;
    }
    else {
-      CARE_STREAM_LOOP(i, start, start + len) {
-         keys[i] = keyResult[i];
-         values[i] = valueResult[i];
+      CARE_STREAM_LOOP(i, 0, len) {
+         keys[i+start] = keyResult[i];
+         values[i+start] = valueResult[i];
       } CARE_STREAM_LOOP_END
 
       if (len > 0) {
