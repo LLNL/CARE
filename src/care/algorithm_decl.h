dcf43fe37e6c9aa0ce00cc96a1ee7047f5260d1f
Benjamin T. Liu <liu15@llnl.gov>
2022-06-07 15:14:48 -0700

    Use refs

--- src/care/algorithm_decl.h
+++ src/care/algorithm_decl.h
@@ -24,19 +24,19 @@
 namespace care {
 
 template <typename T>
-CARE_HOST_DEVICE CARE_INLINE T abs(const T a)
+CARE_HOST_DEVICE CARE_INLINE T abs(const T& a)
 {
    return a > 0 ? a : -a ;
 }
 
 template <typename T>
-CARE_HOST_DEVICE CARE_INLINE T max(const T a, const T b)
+CARE_HOST_DEVICE CARE_INLINE T max(const T& a, const T& b)
 {
    return a > b ? a : b;
 }
 
 template <typename T>
-CARE_HOST_DEVICE CARE_INLINE T min(const T a, const T b)
+CARE_HOST_DEVICE CARE_INLINE T min(const T& a, const T& b)
 {
    return a < b ? a : b;
 }
