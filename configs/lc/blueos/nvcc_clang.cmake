1cb30046c13711aae640809ed53851c9b9508ab7
Alan Dayton <dayton8@llnl.gov>
2023-03-22 16:16:25 -0700

    Rename blueos host config to be more generic

---
+++ configs/lc/blueos/nvcc_clang.cmake
@@ -0,0 +1,9 @@
+set(ENABLE_CUDA ON CACHE BOOL "Enable CUDA")
+set(CUDA_TOOLKIT_ROOT_DIR "/usr/tce/packages/cuda/cuda-11.7.0" CACHE PATH "Path to CUDA")
+set(CUDA_ARCH "sm_70" CACHE STRING "Set the CUDA virtual architecture")
+set(CUDA_CODE "compute_70" CACHE STRING "Set the CUDA actual architecture")
+set(CMAKE_CUDA_ARCHITECTURES "70" CACHE STRING "")
+
+set(NVTOOLSEXT_DIR "${CUDA_TOOLKIT_ROOT_DIR}" CACHE PATH "Path to NVTOOLSEXT")
+
+set(CMAKE_CXX_COMPILER "/usr/tce/packages/clang/clang-ibm-14.0.5/bin/clang++" CACHE FILEPATH "Path to clang++")
