5c800b913571bd34c90b70e3a64dc0a483672120
Alan Dayton <dayton8@llnl.gov>
2023-03-22 16:54:47 -0700

    Fixes for build using submodules on rzansel

--- configs/lc/blueos/nvcc_clang.cmake
+++ configs/lc/blueos/nvcc_clang.cmake
@@ -6,4 +6,9 @@ set(CMAKE_CUDA_ARCHITECTURES "70" CACHE STRING "")
 
 set(NVTOOLSEXT_DIR "${CUDA_TOOLKIT_ROOT_DIR}" CACHE PATH "Path to NVTOOLSEXT")
 
+set(GCC_HOME "/usr/tce/packages/gcc/gcc-8.3.1" CACHE PATH "")
+
 set(CMAKE_CXX_COMPILER "/usr/tce/packages/clang/clang-ibm-14.0.5/bin/clang++" CACHE FILEPATH "Path to clang++")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --gcc-toolchain=${GCC_HOME}" CACHE STRING "C++ compiler flags")
+
+set(BLT_EXPORT_THIRDPARTY OFF CACHE BOOL "")
