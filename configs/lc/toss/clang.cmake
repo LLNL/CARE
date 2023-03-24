c18bd106dd337eee6696fc8f51ea17232ef6b62f
Alan Dayton <dayton8@llnl.gov>
2023-03-24 13:39:08 -0700

    Update toss3 clang host config

--- configs/lc/toss/clang.cmake
+++ configs/lc/toss/clang.cmake
@@ -1,3 +1,7 @@
-set(CMAKE_C_COMPILER "/usr/tce/packages/clang/clang-11.0.1/bin/clang" CACHE FILEPATH "Path to C compiler")
-set(CMAKE_CXX_COMPILER "/usr/tce/packages/clang/clang-11.0.1/bin/clang++" CACHE FILEPATH "Path to C++ compiler")
+set(GCC_HOME "/usr/tce/packages/gcc/gcc-8.3.1" CACHE PATH "")
+
+set(CMAKE_CXX_COMPILER "/usr/tce/packages/clang/clang-14.0.4/bin/clang++" CACHE FILEPATH "Path to clang++")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --gcc-toolchain=${GCC_HOME}" CACHE STRING "C++ compiler flags")
+
+set(BLT_EXPORT_THIRDPARTY OFF CACHE BOOL "")
 
