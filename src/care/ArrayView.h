4aa6a6a2cb310aee40d7ecf4f26ba43416051a43
Alan Dayton <dayton8@llnl.gov>
2023-03-27 10:04:59 -0700

    Decide on convention for array views

--- src/care/ArrayView.h
+++ src/care/ArrayView.h
@@ -6,47 +6,60 @@
 #include "RAJA/util/View.hpp"
 
 namespace care {
-#if defined(CARE_GPUCC)
-   constexpr chai::ExecutionSpace DefaultExecutionSpace = chai::GPU;
-
-   using Layout2D = RAJA::Layout<2, int, 0>;
-#else
-   constexpr chai::ExecutionSpace DefaultExecutionSpace = chai::CPU;
-
-   using Layout2D = RAJA::Layout<2, int, 1>;
-#endif
-
+   ///
+   /// Creates a 1D view on top of a flat allocation (non-owning semantics)
+   ///
+   /// Moves the data to the default execution space (determined at
+   /// configuration time) and wraps it in a RAJA::View.
+   ///
+   /// @param[in] data Container for the allocation
+   /// @param[in] extent Length of the view
+   ///
+   /// @return A RAJA::View object
+   ///
    template <class T>
-   using ArrayView1D = RAJA::View<T, RAJA::Layout<1>>;
+   auto makeArrayView1D(care::host_device_ptr<T> data,
+                        int extent);
 
+   ///
+   /// Creates a 2D view on top of a flat allocation (non-owning semantics)
+   ///
+   /// Moves the data to the default execution space (determined at
+   /// configuration time) and wraps it in a RAJA::View. When using with
+   /// a RAJA loop, extent2 should correspond to the length of the RAJA
+   /// loop for optimal performance.
+   ///
+   /// @param[in] data Container for the allocation
+   /// @param[in] extent1 Length of the 1st dimension
+   /// @param[in] extent2 Length of the 2nd dimension
+   ///
+   /// @return A RAJA::View object
+   ///
    template <class T>
-   using ArrayView2D = RAJA::View<T, Layout2D>;
-
-   template <class T>
-   ArrayView1D<T> makeArrayView1D(care::host_device_ptr<T> data,
-                                  int extent);
-
-   template <class T>
-   ArrayView2D<T> makeArrayView2D(care::host_device_ptr<T> data,
-                                  int extent1, int extent2);
+   auto makeArrayView2D(care::host_device_ptr<T> data,
+                        int extent1, int extent2);
    
    template <class T>
-   ArrayView1D<T> makeArrayView1D(care::host_device_ptr<T> data,
-                                  int extent) {
-      return ArrayView1D<T>(data.data(DefaultExecutionSpace), extent);
+   auto makeArrayView1D(care::host_device_ptr<T> data,
+                        int extent) {
+#if defined(CARE_GPUCC)
+      return RAJA::View<T, RAJA::Layout<1>>(data.data(chai::GPU), extent);
+#else
+      return RAJA::View<T, RAJA::Layout<1>>(data.data(chai::CPU), extent);
+#endif
    }
 
    template <class T>
-   ArrayView2D<T> makeArrayView2D(care::host_device_ptr<T> data,
-                                  int extent1, int extent2) {
+   auto makeArrayView2D(care::host_device_ptr<T> data,
+                        int extent1, int extent2) {
 #if defined(CARE_GPUCC)
-      return ArrayView2D<T>(data.data(DefaultExecutionSpace), extent1, extent2);
+      return RAJA::View<T, RAJA::Layout<2, int, 1>>(data.data(chai::GPU), extent1, extent2);
 #else
       std::array< RAJA::idx_t, 2> perm {{1, 0}};
       RAJA::Layout<2> layout =
          RAJA::make_permuted_layout( {{extent1, extent2}}, perm );
 
-      return ArrayView2D<T>(data.data(DefaultExecutionSpace), layout);
+      return RAJA::View<T, RAJA::Layout<2, int, 0>>(data.data(chai::CPU), layout);
 #endif
    }
 } // namespace care
