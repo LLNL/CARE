90bc99c1361d933f6852426bf09a8648a5a107b3
Alan Dayton <dayton8@llnl.gov>
2023-03-27 15:25:39 -0700

    Update host configs

--- configs/lc/toss/clang.cmake
+++ configs/lc/toss/clang.cmake
@@ -1,7 +1,10 @@
-set(GCC_HOME "/usr/tce/packages/gcc/gcc-8.3.1" CACHE PATH "")
+set(COMPILER_BASE "/usr/tce/packages/clang/clang-14.0.4" CACHE PATH "")
+set(CMAKE_C_COMPILER "${COMPILER_BASE}/bin/clang" CACHE PATH "")
+set(CMAKE_CXX_COMPILER "${COMPILER_BASE}/bin/clang++" CACHE PATH "")
 
-set(CMAKE_CXX_COMPILER "/usr/tce/packages/clang/clang-14.0.4/bin/clang++" CACHE FILEPATH "Path to clang++")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --gcc-toolchain=${GCC_HOME}" CACHE STRING "C++ compiler flags")
+set(GCC_HOME "/usr/tce/packages/gcc/gcc-8.3.1" CACHE PATH "")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} --gcc-toolchain=${GCC_HOME}" CACHE STRING "")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --gcc-toolchain=${GCC_HOME}" CACHE STRING "")
 
 set(BLT_EXPORT_THIRDPARTY OFF CACHE BOOL "")
 
