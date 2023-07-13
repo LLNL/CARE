c123396d0381284a32ae67b52505fe3f87d6d805
Neela Kausik <kausik1@llnl.gov>
2023-07-13 15:42:42 -0700

    maybe silence profileplugin warnings

--- src/care/ProfilePlugin.cpp
+++ src/care/ProfilePlugin.cpp
@@ -28,22 +28,20 @@ namespace care{
    void ProfilePlugin::preLaunch(const RAJA::util::PluginContext& p) {
 #if defined(__CUDACC__) && CARE_HAVE_NVTOOLSEXT
       // Profile the host loops
-      if (s_profile_host_loops) {
-         if (p.platform == RAJA::Platform::host) {
-            std::string name = PluginData::getFileName() + std::to_string(PluginData::getLineNumber());
-            int color_id = s_current_color + 1;
-            color_id = color_id % s_num_colors;
+      if (s_profile_host_loops && p.platform == RAJA::Platform::host) {
+         std::string name = PluginData::getFileName() + std::to_string(PluginData::getLineNumber());
+         int color_id = s_current_color + 1;
+         color_id = color_id % s_num_colors;
          
-            // TODO: Add error checking
-            nvtxEventAttributes_t eventAttrib = { 0 };
-            eventAttrib.version = NVTX_VERSION;
-            eventAttrib.size = NVTX_EVENT_ATTRIB_STRUCT_SIZE;
-            eventAttrib.colorType = NVTX_COLOR_ARGB;
-            eventAttrib.color = s_colors[color_id];
-            eventAttrib.messageType = NVTX_MESSAGE_TYPE_ASCII;
-            eventAttrib.message.ascii = name.c_str();
-            nvtxRangePushEx(&eventAttrib);
-         }
+         // TODO: Add error checking
+         nvtxEventAttributes_t eventAttrib = { 0 };
+         eventAttrib.version = NVTX_VERSION;
+         eventAttrib.size = NVTX_EVENT_ATTRIB_STRUCT_SIZE;
+         eventAttrib.colorType = NVTX_COLOR_ARGB;
+         eventAttrib.color = s_colors[color_id];
+         eventAttrib.messageType = NVTX_MESSAGE_TYPE_ASCII;
+         eventAttrib.message.ascii = name.c_str();
+         nvtxRangePushEx(&eventAttrib);
       }
 #endif // defined(__CUDACC__) && CARE_HAVE_NVTOOLSEXT
    }
@@ -51,13 +49,10 @@ namespace care{
 
    void ProfilePlugin::postLaunch(const RAJA::util::PluginContext& p) {
 #if defined(__CUDACC__) && CARE_HAVE_NVTOOLSEXT
-      if (s_profile_host_loops) {
-         if (p.platform == RAJA::Platform::host) {
-            // TODO: Add error checking
-            nvtxRangePop();
-         }
+      if (s_profile_host_loops && p.platform == RAJA::Platform::host) {
+         // TODO: Add error checking
+         nvtxRangePop();
       }
 #endif
-
    }
 }
