f12d735cc5eb161c7e297702c57b01950195ea74
Alan Dayton <dayton8@llnl.gov>
2023-07-19 08:38:41 -0700

    Add test case

--- test/TestCudaUmpireResource.cpp
+++ test/TestCudaUmpireResource.cpp
@@ -11,22 +11,60 @@
 
 // other library headers
 #include "gtest/gtest.h"
+#include "umpire/strategy/QuickPool.hpp"
 
 // care headers
 #include "care/CudaUmpireResource.h"
-#include "care/DefaultMacros.h"
-#include "care/policies.h"
 #include "care/detail/test_utils.h"
 
 GPU_TEST(CudaUmpireResource, gpu_initialization) {
-   printf("Initializing\n");
    init_care_for_testing();
-   printf("Initialized... Testing care::array\n");
 }
 
 GPU_TEST(CudaUmpireResource, DefaultConstructor)
 {
    care::CudaUmpireResource resource;
+}
+
+GPU_TEST(CudaUmpireResource, AllocatorConstructor)
+{
+   auto& rm = umpire::ResourceManager::getInstance();
+
+   // Device allocator
+   auto deviceAllocator = rm.getAllocator("DEVICE_POOL"); // Initialized above
+   auto customDeviceAllocator =
+      rm.makeAllocator<umpire::strategy::QuickPool>("CUSTOM_DEVICE_POOL",
+                                                    deviceAllocator,
+                                                    64*1024*1024,
+                                                    16*1024*1024);
+
+   // Pinned allocator
+   auto pinnedAllocator = rm.getAllocator("PINNED_POOL"); // Initialized above
+   auto customPinnedAllocator =
+      rm.makeAllocator<umpire::strategy::QuickPool>("CUSTOM_PINNED_POOL",
+                                                    pinnedAllocator,
+                                                    8*1024*1024,
+                                                    2*1024*1024);
+
+   // Managed allocator
+   auto managedAllocator = rm.getAllocator("UM"); // Umpire default
+
+   // Make a unified memory pool to draw from (not done in init_care_for_testing())
+   auto managedPoolAllocator =
+      rm.makeAllocator<umpire::strategy::QuickPool>("UM_POOL",
+                                                    managedAllocator,
+                                                    128*1024*1024,
+                                                    8*1024*1024);
+
+   auto customManagedAllocator =
+      rm.makeAllocator<umpire::strategy::QuickPool>("CUSTOM_UM_POOL",
+                                                    managedPoolAllocator,
+                                                    8*1024*1024,
+                                                    2*1024*1024);
+
+   care::CudaUmpireResource resource(customDeviceAllocator,
+                                     customPinnedAllocator,
+                                     customManagedAllocator);
 }
 
 #endif // CARE_GPUCC
