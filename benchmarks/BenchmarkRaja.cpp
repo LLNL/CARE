0459805e22d6bd4b33d13dc9064a32562a6ec847
Neela Kausik <kausik1@llnl.gov>
2023-08-03 13:19:24 -0700

    fixed loop size

--- benchmarks/BenchmarkRaja.cpp
+++ benchmarks/BenchmarkRaja.cpp
@@ -20,48 +20,47 @@
 #include <climits>
 #include <cmath>
 
-#define NUM_KERNELS 4
+#define size 1000000
 
-using namespace care;
+namespace care{
 
 #if defined(CARE_GPUCC)
 //each kernel has a separate stream
 static void benchmark_gpu_loop_separate_streams(benchmark::State& state) {
    int N = state.range(0);
-
-   RAJA::resources::Cuda res_arr[NUM_KERNELS];
-   RAJA::resources::Event event_arr[NUM_KERNELS];
-   care::host_device_ptr<int> arrays[NUM_KERNELS];
-   for(int i = 0; i < NUM_KERNELS; i++)
+   RAJA::resources::Cuda res_arr[N];
+   RAJA::resources::Event event_arr[N];
+   care::host_device_ptr<int> arrays[16];
+   for(int i = 0; i < N; i++)
    {
       RAJA::resources::Cuda res;
       res_arr[i] = res;
       RAJA::resources::Event e = res.get_event();
       event_arr[i] = e;
-      care::host_device_ptr<int> arr(N, "arr");
+      care::host_device_ptr<int> arr(size, "arr");
       arrays[i] = arr;
    }
 
    //warmup kernel
    RAJA::resources::Cuda warmup_res;
-   CARE_STREAMED_LOOP(warmup_res, i, 0 , N) {
+   CARE_STREAMED_LOOP(warmup_res, i, 0 , size) {
       arrays[0][i] = 0;
    } CARE_STREAMED_LOOP_END					
 	
    for (auto _ : state) {
       //run num kernels
-      for(int j = 0; j < NUM_KERNELS; j++)
+      for(int j = 0; j < N; j++)
       {
-         CARE_STREAMED_LOOP(res_arr[j], i, 0 , N) {
+         CARE_STREAMED_LOOP(res_arr[j], i, 0 , size) {
             arrays[j][i] = i;
          } CARE_STREAMED_LOOP_END					
       }
    }
-   for(int i = 0; i < NUM_KERNELS; i++) {arrays[i].free();}
+   for(int i = 0; i < N; i++) {arrays[i].free();}
 }
 
 // Register the function as a benchmark
-BENCHMARK(benchmark_gpu_loop_separate_streams)->Range(1, INT_MAX);
+BENCHMARK(benchmark_gpu_loop_separate_streams)->Arg(1)->Arg(2)->Arg(4)->Arg(8)->Arg(12)->Arg(16);
 
 //all kernels on one stream
 static void benchmark_gpu_loop_single_stream(benchmark::State& state) {
@@ -69,36 +68,38 @@ static void benchmark_gpu_loop_single_stream(benchmark::State& state) {
 
    RAJA::resources::Cuda res;   
 
-   care::host_device_ptr<int> arrays[NUM_KERNELS];
-   for(int i = 0; i < NUM_KERNELS; i++)
+   care::host_device_ptr<int> arrays[16];
+   for(int i = 0; i < N; i++)
    {
-      care::host_device_ptr<int> arr(N, "arr");
+      care::host_device_ptr<int> arr(size, "arr");
       arrays[i] = arr;
    }
 
    //warmup kernel
    RAJA::resources::Cuda warmup_res;
-   CARE_STREAMED_LOOP(warmup_res, i, 0, N) {
+   CARE_STREAMED_LOOP(warmup_res, i, 0, size) {
       arrays[0][i] = i;
    } CARE_STREAMED_LOOP_END
 
    for (auto _ : state) {
       //run num kernels
-      for(int j = 0; j < NUM_KERNELS; j++)
+      for(int j = 0; j < N; j++)
       {
-         CARE_STREAMED_LOOP(res, i, 0, N) {
+         CARE_STREAMED_LOOP(res, i, 0, size) {
             arrays[j][i] = i;
          } CARE_STREAMED_LOOP_END
          res.wait();
       }
    }
-   for(int i = 0; i < NUM_KERNELS; i++) {arrays[i].free();}
+   for(int i = 0; i < N; i++) {arrays[i].free();}
 }
 
 // Register the function as a benchmark
-BENCHMARK(benchmark_gpu_loop_single_stream)->Range(1, INT_MAX);
+BENCHMARK(benchmark_gpu_loop_single_stream)->Arg(1)->Arg(2)->Arg(4)->Arg(8)->Arg(12)->Arg(16);
 
 #endif
+
+} //namespace care
 
 // Run the benchmarks
 BENCHMARK_MAIN();
