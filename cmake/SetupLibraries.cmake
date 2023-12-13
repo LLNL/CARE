07f44f0d37858cf90ffa3b3f14b3bb687a884d9f
Alan Dayton <dayton8@llnl.gov>
2023-12-13 10:02:11 -0800

    Clean up CMake

--- cmake/SetupLibraries.cmake
+++ cmake/SetupLibraries.cmake
@@ -9,7 +9,7 @@
 # CAMP (required)
 ################################
 if (NOT TARGET camp)
-   find_package(camp QUIET NO_DEFAULT_PATH HINTS ${CAMP_DIR} ${camp_DIR})
+   find_package(camp QUIET NO_DEFAULT_PATH PATHS ${CAMP_DIR})
 
    if (camp_FOUND)
       message(STATUS "CARE: Using external CAMP")
@@ -18,7 +18,7 @@ if (NOT TARGET camp)
       message(STATUS "CARE: Using CAMP submodule")
 
       if (NOT EXISTS ${PROJECT_SOURCE_DIR}/tpl/camp/CMakeLists.txt)
-         message(FATAL_ERROR "CARE: CAMP submodule not initialized. Run 'git submodule update --init' in the git repository or set camp_DIR or CAMP_DIR to use an external build of CAMP.")
+         message(FATAL_ERROR "CARE: CAMP submodule not initialized. Run 'git submodule update --init' in the git repository or set CAMP_DIR to use an external build of CAMP.")
       else ()
          add_subdirectory(${PROJECT_SOURCE_DIR}/tpl/camp)
       endif ()
@@ -47,7 +47,7 @@ endif ()
 # Umpire (required)
 ################################
 if (NOT TARGET umpire)
-   find_package(umpire QUIET NO_DEFAULT_PATH HINTS ${UMPIRE_DIR} ${umpire_DIR})
+   find_package(umpire QUIET NO_DEFAULT_PATH PATHS ${UMPIRE_DIR})
 
    if (umpire_FOUND)
       message(STATUS "CARE: Using external Umpire")
@@ -55,7 +55,7 @@ if (NOT TARGET umpire)
       message(STATUS "CARE: Using Umpire submodule")
 
       if (NOT EXISTS ${PROJECT_SOURCE_DIR}/tpl/umpire/CMakeLists.txt)
-         message(FATAL_ERROR "CARE: Umpire submodule not initialized. Run 'git submodule update --init' in the git repository or set umpire_DIR or UMPIRE_DIR to use an external build of Umpire.")
+         message(FATAL_ERROR "CARE: Umpire submodule not initialized. Run 'git submodule update --init' in the git repository or set UMPIRE_DIR to use an external build of Umpire.")
       else ()
          set(UMPIRE_ENABLE_PINNED ${ENABLE_PINNED} CACHE BOOL "Enable pinned memory")
 
@@ -78,7 +78,7 @@ endif ()
 # RAJA (required)
 ################################
 if (NOT TARGET RAJA)
-   find_package(raja QUIET NO_DEFAULT_PATH HINTS ${RAJA_DIR} ${raja_DIR})
+   find_package(raja QUIET NO_DEFAULT_PATH PATHS ${RAJA_DIR})
 
    if (raja_FOUND)
       message(STATUS "CARE: Using external RAJA")
@@ -86,7 +86,7 @@ if (NOT TARGET RAJA)
       message(STATUS "CARE: Using RAJA submodule")
 
       if (NOT EXISTS ${PROJECT_SOURCE_DIR}/tpl/raja/CMakeLists.txt)
-         message(FATAL_ERROR "CARE: RAJA submodule not initialized. Run 'git submodule update --init' in the git repository or set raja_DIR or RAJA_DIR to use an external build of RAJA.")
+         message(FATAL_ERROR "CARE: RAJA submodule not initialized. Run 'git submodule update --init' in the git repository or set RAJA_DIR to use an external build of RAJA.")
       else ()
          set(RAJA_ENABLE_TESTS ${CARE_ENABLE_SUBMODULE_TESTS} CACHE BOOL "Enable RAJA tests")
          set(RAJA_ENABLE_BENCHMARKS ${CARE_ENABLE_SUBMODULE_BENCHMARKS} CACHE BOOL "Enable RAJA benchmarks")
@@ -121,7 +121,7 @@ endif ()
 # CHAI (required)
 ################################
 if (NOT TARGET chai)
-   find_package(chai QUIET NO_DEFAULT_PATH HINTS ${CHAI_DIR} ${chai_DIR})
+   find_package(chai QUIET NO_DEFAULT_PATH PATHS ${CHAI_DIR})
 
    if (chai_FOUND)
       message(STATUS "CARE: Using external CHAI")
@@ -129,7 +129,7 @@ if (NOT TARGET chai)
       message(STATUS "CARE: Using CHAI submodule")
 
       if (NOT EXISTS ${PROJECT_SOURCE_DIR}/tpl/chai/CMakeLists.txt)
-         message(FATAL_ERROR "CARE: CHAI submodule not initialized. Run 'git submodule update --init' in the git repository or set chai_DIR or CHAI_DIR to use an external build of CHAI.")
+         message(FATAL_ERROR "CARE: CHAI submodule not initialized. Run 'git submodule update --init' in the git repository or set CHAI_DIR to use an external build of CHAI.")
       else ()
          set(CHAI_ENABLE_PICK ${ENABLE_PICK} CACHE BOOL "Enable picks/sets in chai::ManagedArray")
          set(CHAI_ENABLE_PINNED ${ENABLE_PINNED} CACHE BOOL "Enable pinned memory support in CHAI")
@@ -155,7 +155,7 @@ endif ()
 ################################
 if (NOT TARGET LLNL_GlobalID::LLNL_GlobalID)
    if (LLNL_GLOBALID_DIR)
-      find_package(LLNL_GlobalID REQUIRED NO_DEFAULT_PATH HINTS ${LLNL_GLOBALID_DIR} ${LLNL_GlobalID_DIR})
+      find_package(LLNL_GlobalID REQUIRED NO_DEFAULT_PATH PATHS ${LLNL_GLOBALID_DIR})
 
       # Manually set includes as system includes
       get_target_property(_dirs LLNL_GlobalID::LLNL_GlobalID INTERFACE_INCLUDE_DIRECTORIES)
@@ -171,17 +171,17 @@ if (NOT TARGET LLNL_GlobalID::LLNL_GlobalID)
 endif()
 
 ################################
+# CUB (required for CUDA build)
+################################
+if (ENABLE_CUDA AND NOT TARGET cub)
+   include(${CMAKE_CURRENT_LIST_DIR}/libraries/FindCUB.cmake)
+endif ()
+
+################################
 # NVTOOLSEXT
 ################################
 if (ENABLE_CUDA AND NOT TARGET CUDA::nvToolsExt)
    set(CUDAToolkit_ROOT ${CUDA_TOOLKIT_ROOT_DIR})
    find_package(CUDAToolkit)
-endif ()
-
-################################
-# CUB (required for CUDA build)
-################################
-if (ENABLE_CUDA AND NOT TARGET cub)
-   include(cmake/libraries/FindCUB.cmake)
 endif ()
 
