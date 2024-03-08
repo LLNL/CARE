a384a63acdb152c9f4852d1b97fd2ee475972c46
Alan Dayton <dayton8@llnl.gov>
2024-03-08 11:55:49 -0800

    Improve license check

--- scripts/check-license-info.sh
+++ scripts/check-license-info.sh
@@ -15,9 +15,51 @@ RED="\033[1;31m"
 GREEN="\033[1;32m"
 NOCOLOR="\033[0m"
 
-#files_no_license=$(grep -rL "LLC and CARE" . \
-#files_no_license=$(grep -rL "SPDX-License-Identifier: BSD-3-Clause" . \
-files_no_license=$(grep -rL "the CARE LICENSE file" . \
+files_no_license=$(grep -rL "LLC and CARE" . \
+   --exclude-dir=.git \
+   --exclude-dir=blt \
+   --exclude-dir=cub \
+   --exclude-dir=umpire \
+   --exclude-dir=raja \
+   --exclude-dir=chai \
+   --exclude-dir=radiuss-spack-configs \
+   --exclude-dir=uberenv \
+   --exclude-dir=build \
+   --exclude=\*.swp \
+   --exclude=ci.yml \
+   --exclude=.gitignore \
+   --exclude=.gitmodules \
+   --exclude=LICENSE \
+   --exclude=COPYRIGHT \
+   --exclude=NOTICE \
+   --exclude=\*.natvis \
+   --exclude=repo.yaml \
+   --exclude=package.py \
+   --exclude=.uberenv_config.json)
+
+files_no_license+=$(grep -rL "the CARE LICENSE file" . \
+   --exclude-dir=.git \
+   --exclude-dir=blt \
+   --exclude-dir=cub \
+   --exclude-dir=umpire \
+   --exclude-dir=raja \
+   --exclude-dir=chai \
+   --exclude-dir=radiuss-spack-configs \
+   --exclude-dir=uberenv \
+   --exclude-dir=build \
+   --exclude=\*.swp \
+   --exclude=ci.yml \
+   --exclude=.gitignore \
+   --exclude=.gitmodules \
+   --exclude=LICENSE \
+   --exclude=COPYRIGHT \
+   --exclude=NOTICE \
+   --exclude=\*.natvis \
+   --exclude=repo.yaml \
+   --exclude=package.py \
+   --exclude=.uberenv_config.json)
+
+files_no_license+=$(grep -rL "SPDX-License-Identifier: BSD-3-Clause" . \
    --exclude-dir=.git \
    --exclude-dir=blt \
    --exclude-dir=cub \
@@ -40,7 +82,7 @@ files_no_license=$(grep -rL "the CARE LICENSE file" . \
    --exclude=.uberenv_config.json)
 
 if [ $files_no_license ]; then
-  print "${RED} [!] Some files are missing license text: ${NOCOLOR}"
+  print "${RED} [!] Some files are missing license text:${NOCOLOR}"
   echo "${files_no_license}"
   exit 255
 else
