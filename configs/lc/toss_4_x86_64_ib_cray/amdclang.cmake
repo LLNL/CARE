d9075cd47803e06a883609800053e96ac6a915e4
Alan Dayton <dayton8@llnl.gov>
2024-04-11 11:55:08 -0700

    Add an amdclang host config

---
+++ configs/lc/toss_4_x86_64_ib_cray/amdclang.cmake
@@ -0,0 +1,14 @@
+##############################################################################
+# Copyright (c) 2020-24, Lawrence Livermore National Security, LLC and CARE
+# project contributors. See the CARE LICENSE file for details.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+##############################################################################
+
+set(COMPILER_BASE "/usr/tce/packages/rocmcc/rocmcc-6.0.3-magic" CACHE PATH "")
+set(CMAKE_C_COMPILER "${COMPILER_BASE}/bin/amdclang" CACHE PATH "")
+set(CMAKE_CXX_COMPILER "${COMPILER_BASE}/bin/amdclang++" CACHE PATH "")
+
+set(ENABLE_HIP ON CACHE BOOL "Enable Hip")
+set(ROCM_PATH "/usr/tce/packages/rocmcc/rocmcc-6.0.3-magic" CACHE PATH "")
+set(CMAKE_HIP_ARCHITECTURES "gfx942:xnack+" CACHE STRING "")
