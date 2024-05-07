d56d713e7c4eeb9d6e490c9bc8d829a78a7dc87b
Alan Dayton <dayton8@llnl.gov>
2024-05-06 17:41:07 -0700

    Simplify reproducer

--- reproducers/ReproducerArray.cpp
+++ reproducers/ReproducerArray.cpp
@@ -25,42 +25,29 @@
 #include "care/array.h"
 
 int main(int, char**) {
-   care::array<int, 2> a{{3, 7}};
-   RAJAReduceMin<bool> passed{true};
-
+   // Array containing host_device_ptr
 #if WANT_EXIT_CODE_139
-   CARE_REDUCE_LOOP(i, 0, 1) {
-      if (a.front() != 3) {
-         passed.min(false);
-         return;
-      }
-   } CARE_REDUCE_LOOP_END
-
-   if ((bool) passed) {
-      std::cout << "Kernel before\n";
-   }
+   care::array<care::host_device_ptr<int>, 1> a{{nullptr}};
+#else
+   care::array<care::host_device_ptr<int>, 1> a;
 #endif
 
-   // Array containing host_device_ptr
-   care::array<care::host_device_ptr<int>, 1> b;
-
    for (int i = 0; i < 1; ++i) {
-      b[i] = care::host_device_ptr<int>(10);
+      a[i] = care::host_device_ptr<int>(10);
    }
 
    CARE_STREAM_LOOP(i, 0, 10) {
-      b[0][i] = i;
+      a[0][i] = i;
    } CARE_STREAM_LOOP_END
 
-   b[0].free();
+   a[0].free();
 
    // Kernel afterwards
-#if WANT_EXIT_CODE_139
-   passed.reset(true);
-#endif
+   care::array<int, 2> b{{3, 7}};
+   RAJAReduceMin<bool> passed{true};
 
    CARE_REDUCE_LOOP(i, 0, 1) {
-      if (a.back() != 7) {
+      if (b.back() != 7) {
          passed.min(false);
       }
    } CARE_REDUCE_LOOP_END
