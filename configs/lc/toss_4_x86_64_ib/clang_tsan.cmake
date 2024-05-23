a92078949acf332f329917d0a2dc54ce67ffb012
Alan Dayton <dayton8@llnl.gov>
2024-05-23 14:36:56 -0700

    Let CMake add in the optimization flags

--- configs/lc/toss_4_x86_64_ib/clang_tsan.cmake
+++ configs/lc/toss_4_x86_64_ib/clang_tsan.cmake
@@ -10,8 +10,8 @@ set(CMAKE_C_COMPILER "${COMPILER_BASE}/bin/clang" CACHE PATH "")
 set(CMAKE_CXX_COMPILER "${COMPILER_BASE}/bin/clang++" CACHE PATH "")
 
 set(GCC_HOME "/usr/tce/packages/gcc/gcc-10.3.1-magic" CACHE PATH "")
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} --gcc-toolchain=${GCC_HOME} -fsanitize=thread -g -O1" CACHE STRING "")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --gcc-toolchain=${GCC_HOME} -fsanitize=thread -g -O1" CACHE STRING "")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} --gcc-toolchain=${GCC_HOME} -fsanitize=thread -g" CACHE STRING "")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --gcc-toolchain=${GCC_HOME} -fsanitize=thread -g" CACHE STRING "")
 set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fsanitize=thread" CACHE STRING "")
 
 set(ENABLE_OPENMP ON CACHE BOOL "")
