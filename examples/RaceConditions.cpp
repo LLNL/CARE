a2f39fb94187c54122954f1bf73cd1c95ba189bc
Alan Dayton <dayton8@llnl.gov>
2024-05-23 14:52:12 -0700

    Update race conditions example

--- examples/RaceConditions.cpp
+++ examples/RaceConditions.cpp
@@ -17,10 +17,18 @@
 
 int main(int, char**) {
    int size = 10;
-   care::host_device_ptr<int> data(1);
+   care::host_device_ptr<int> data(size);
 
    CARE_OPENMP_LOOP(i, 0, size) {
-      data[0] += i;
+      data[0] += i; // clang tsan catches this
+   } CARE_OPENMP_LOOP_END
+
+   CARE_OPENMP_LOOP(i, 0, size - 1) {
+      data[i] = data[i + 1]; // clang tsan catches this
+   } CARE_OPENMP_LOOP_END
+
+   CARE_OPENMP_LOOP(i, 0, size) {
+      data[0] = 1; // clang tsan does not report
    } CARE_OPENMP_LOOP_END
 
    data.free();
