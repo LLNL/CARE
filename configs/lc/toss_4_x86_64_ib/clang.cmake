81f10e856653382f514e014f33904d14fd43d996
Alan Dayton <6393677+adayton1@users.noreply.github.com>
2024-06-10 12:23:45 -0700

    Build fixes (#264)
    * Fix conflicting variable name in CMake
    * Update gcc version in host config
    * Make sure hip offload architecture gets passed correctly

--- configs/lc/toss_4_x86_64_ib/clang.cmake
+++ configs/lc/toss_4_x86_64_ib/clang.cmake
@@ -9,9 +9,8 @@ set(COMPILER_BASE "/usr/tce/packages/clang/clang-14.0.6-magic" CACHE PATH "")
 set(CMAKE_C_COMPILER "${COMPILER_BASE}/bin/clang" CACHE PATH "")
 set(CMAKE_CXX_COMPILER "${COMPILER_BASE}/bin/clang++" CACHE PATH "")
 
-set(GCC_HOME "/usr/tce/packages/gcc/gcc-10.3.1-magic" CACHE PATH "")
+set(GCC_HOME "/usr/tce/packages/gcc/gcc-12.1.1-magic" CACHE PATH "")
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} --gcc-toolchain=${GCC_HOME}" CACHE STRING "")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --gcc-toolchain=${GCC_HOME}" CACHE STRING "")
 
 set(BLT_EXPORT_THIRDPARTY OFF CACHE BOOL "")
-
