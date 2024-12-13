8a054d390b80c630054f093f2a872c2de2b147e1
Ben Liu <38140930+liu15@users.noreply.github.com>
2024-12-13 11:40:32 -0800

    Add additional REDUCE macros (#297)
    * Add additional REDUCE macros
    
    * Use gpu_reduce instead of parallel_reduce
    
    ---------
    
    Co-authored-by: Alan Dayton <6393677+adayton1@users.noreply.github.com>

--- src/care/policies.h
+++ src/care/policies.h
@@ -14,8 +14,8 @@ namespace care {
    struct sequential {};
    struct openmp {};
    struct gpu {};
+   struct gpu_reduce {};
    struct parallel {};
-   struct parallel_reduce {};
    struct raja_fusible {};
    struct raja_fusible_seq {};
    struct managed_ptr_read {};
@@ -27,8 +27,8 @@ namespace care {
       sequential,
       openmp,
       gpu,
+      gpu_reduce,
       parallel,
-      parallel_reduce,
       managed_ptr_read,
       managed_ptr_write
    };
