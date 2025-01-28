cc4e20aa068b9bf28e8ddefa362aedc428df2c18
Alan Dayton <dayton8@llnl.gov>
2025-01-28 11:41:32 -0800

    Commit comment

--- examples/DeviceASAN.cpp
+++ examples/DeviceASAN.cpp
@@ -16,6 +16,12 @@ int main(int, char**) {
       data[i] = i;
    } CARE_STREAM_LOOP_END
 
+   // This will write past the end of the array.
+   // ASAN should report something like the following:
+   //
+   // ==4084715==ERROR: AddressSanitizer: heap-buffer-overflow on amdgpu device 0 at pc 0x153d3cdd3db8
+   // READ of size 4 in workgroup id (3906,0,0)
+   //   #0 0x153d3cdd3db8 in main::'lambda0'(int)::operator()(int) const at /g/g17/dayton8/ale3d/github_care_4/examples/DeviceASAN.cpp:26:14
    CARE_STREAM_LOOP(i, 0, size + 1) {
       data[i]++;
    } CARE_STREAM_LOOP_END
