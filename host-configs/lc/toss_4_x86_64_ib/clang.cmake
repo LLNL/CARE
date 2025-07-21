ff701d9fe8dfb5311348ce4df91cdc8935b29c68
Alan Dayton <6393677+adayton1@users.noreply.github.com>
2025-07-21 14:23:34 -0700

    Update host configs (#339)
    * Move configs to host-configs directory for consistency with other projects
    
    * Update from ROCm 6.3.0 to 6.4.1, remove xnack+ from architecture specification, and specify gcc toolchain explicitly
    
    * Update to GCC 13.3.1 std libraries

--- host-configs/lc/toss_4_x86_64_ib/clang.cmake
+++ host-configs/lc/toss_4_x86_64_ib/clang.cmake
@@ -5,13 +5,12 @@
 # SPDX-License-Identifier: BSD-3-Clause
 ##############################################################################
 
-set(COMPILER_BASE "/usr/tce/packages/intel/intel-2022.1.0-magic" CACHE PATH "")
-set(CMAKE_C_COMPILER "${COMPILER_BASE}/bin/icx" CACHE PATH "")
-set(CMAKE_CXX_COMPILER "${COMPILER_BASE}/bin/icpx" CACHE PATH "")
+set(COMPILER_BASE "/usr/tce/packages/clang/clang-14.0.6-magic" CACHE PATH "")
+set(CMAKE_C_COMPILER "${COMPILER_BASE}/bin/clang" CACHE PATH "")
+set(CMAKE_CXX_COMPILER "${COMPILER_BASE}/bin/clang++" CACHE PATH "")
 
-set(GCC_HOME "/usr/tce/packages/gcc/gcc-12.1.1-magic" CACHE PATH "")
+set(GCC_HOME "/usr/tce/packages/gcc/gcc-13.3.1-magic" CACHE PATH "")
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} --gcc-toolchain=${GCC_HOME}" CACHE STRING "")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --gcc-toolchain=${GCC_HOME}" CACHE STRING "")
 
 set(BLT_EXPORT_THIRDPARTY OFF CACHE BOOL "")
-
